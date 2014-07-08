6a61e65ac8ccc95e870628e129fa409310da8bd9
Bill Keese <bill@dojotoolkit.org>
2014-07-08 20:28:18 +0900

    Make decor/Evented module to replace dojo/Evented.
    There are a lot of ways to make this module.  I copied the technique from
    dojo, but we could alternately:
    
    1. emit events on the document itself, and then use
       addEventListener()/removeEventListener() rather than dcl.advise()
    2. hand craft the code to keep track of who is listening for what event

---
+++ Evented.js
@@ -0,0 +1,39 @@
+/** @module decor/Evented */
+define(["dcl/dcl", "dcl/advise"], function (dcl, advise) {
+	/**
+	 * Base class to add on() and emit() methods to a class for listening for events and emitting events:
+	 * @example
+	 * var EventedSubClass = dcl(Evented, {...});
+	 * var instance = new EventedSubClass();
+	 * instance.on("open", function (event) {
+	 *     ... do something with event
+	 * });
+	 * instance.emit("open", {name:"some event", ...});
+	 * @mixin module:delite/Container
+	 * @augments module:delite/Widget
+	 */
+	return dcl(null, {
+		/**
+		 * Setup listener to be called when specified event is fired.
+		 * @param {string} type - Name of event.
+		 * @param {Function} listener - Callback for when event occurs.
+		 * @returns {Object} Handle with `remove()` method to stop listening to event.
+		 */
+		on: function (type, listener) {
+			return advise.before(this, "on" + type, listener);
+		},
+
+		/**
+		 * Emit specified event.
+		 * @param {string} type - Name of event.
+		 * @param {...anything} var_args Parameters to pass to the listeners for this event.
+		 */
+		emit: function (type) {
+			var func = "on" + type;
+			if (this[func]) {
+				var args = Array.prototype.slice.call(arguments, 1);
+				this[func].apply(this, args);
+			}
+		}
+	});
+});
