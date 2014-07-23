d66515fddcebcab75b85d37fb1daa9d0207fbc48
Bill Keese <bill@dojotoolkit.org>
2014-07-24 05:17:52 +0900

    fix Evented API doc, fixes #18

--- Evented.js
+++ Evented.js
@@ -1,23 +1,22 @@
 /** @module decor/Evented */
 define(["dcl/dcl", "dcl/advise"], function (dcl, advise) {
 	/**
-	 * Base class to add on() and emit() methods to a class for listening for events and emitting events:
+	 * Base class to add `on()` and `emit()` methods to a class for listening for events and emitting events.
 	 * @example
 	 * var EventedSubClass = dcl(Evented, {...});
 	 * var instance = new EventedSubClass();
 	 * instance.on("open", function (event) {
 	 *     ... do something with event
 	 * });
-	 * instance.emit("open", {name:"some event", ...});
-	 * @mixin module:delite/Container
-	 * @augments module:delite/Widget
+	 * instance.emit("open", {name: "some event", ...});
+	 * @mixin module:decor/Evented
 	 */
-	return dcl(null, {
+	return dcl(null, /** @lends module:decor/Evented# */ {
 		/**
 		 * Setup listener to be called when specified event is fired.
 		 * @param {string} type - Name of event.
 		 * @param {Function} listener - Callback for when event occurs.
-		 * @returns {Object} Handle with `remove()` method to stop listening to event.
+		 * @returns {Object} Handle with `destroy()` method to stop listening to event.
 		 */
 		on: function (type, listener) {
 			return advise.before(this, "on" + type, listener);
