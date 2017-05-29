5f587c37d773dda9351a6e4acf5c0f4a7e0ef72c
Bill Keese <bill@dojotoolkit.org>
2017-05-30 00:27:12 +0900

    observe() deep support
    Refs #60.

--- observe.js
+++ observe.js
@@ -6,6 +6,116 @@ define([
 	dcl,
 	schedule
 ) {
+	// Object.is() polyfill from Observable.js
+	function is(lhs, rhs) {
+		return lhs === rhs && (lhs !== 0 || 1 / lhs === 1 / rhs) || lhs !== lhs && rhs !== rhs;
+	}
+
+	// Deep compare oldObj to newObj and call notify() for properties that were changed or added in newObj.
+	function diff(oldObj, newObj, notify, prefix) {
+		for (var prop in newObj) {
+			if (!oldObj || !(prop in oldObj) || !is(oldObj[prop], newObj[prop])) {
+				if (newObj[prop] && typeof newObj[prop] === "object") {
+					diff(oldObj && oldObj[prop], newObj[prop], notify, prefix + prop + ".");
+				} else {
+					notify(prefix + prop, oldObj ? oldObj[prop] : undefined, newObj[prop]);
+				}
+			}
+		}
+	}
+
+	// Mapping from instrumented POJO to array of listeners to notify when that object is changed.
+	var map = new WeakMap();
+
+	// Call callback(prop, oldVal, newVal) whenever a property or nested property of the POJO is changed.
+	// Callback is synchronous.
+	function watchPojo(pojo, callback) {
+		// Array of functions to call whenever a property or nested property of the POJO is changed.
+		var callbacks = map.get(pojo);
+
+		if (!callbacks) {
+			callbacks = [];
+			map.set(pojo, callbacks);
+
+			// Go through each property in the object, and convert it to a custom setter and getter.
+			Object.keys(pojo).forEach(function (prop) {
+				// Shadow value referenced by setter and getter.
+				var curVal = pojo[prop];
+
+				// If property is an object then set up listener on that object too.
+				// Note: In the future, may want to exclude arrays?  They could be long.
+				var nestedWatcher;
+				if (curVal && typeof curVal === "object") {
+					nestedWatcher = watchPojo(curVal, function (nestedProp, nestedOldVal, nestedNewVal) {
+						callback(prop + "." + nestedProp, nestedOldVal, nestedNewVal);
+					});
+				}
+
+				// Convert property into setter and getter.
+				Object.defineProperty(pojo, prop, {
+					enumerable: true,
+
+					set: function (newVal) {
+						// Ignore when property set to same value as before.
+						if (is(newVal, curVal)) {
+							return;
+						}
+
+						// If old value was an object, then remove listener on that object.
+						if (nestedWatcher) {
+							nestedWatcher.remove();
+							nestedWatcher = null;
+						}
+
+						// If new value is an object, set up listener on that object.
+						if (newVal && typeof newVal === "object") {
+							nestedWatcher = watchPojo(newVal, function (nestedProp, nestedOldVal, nestedNewVal) {
+								callback(prop + "." + nestedProp, nestedOldVal, nestedNewVal);
+							});
+						}
+
+						// Save new value.
+						var oldVal = curVal;
+						curVal = newVal;
+
+						function notify(prop, oldVal, newVal) {
+							callbacks.forEach(function (watcher) {
+								watcher(prop, oldVal, newVal);
+							});
+						}
+
+						if (newVal && typeof newVal === "object") {
+							// Recursive diff oldVal vs. newVal and send notifications of nested prop changes.
+							diff(oldVal, newVal, notify, prop + ".");
+						} else {
+							// Notify all listeners that property has changed.
+							notify(prop, oldVal, newVal);
+						}
+					},
+
+					get: function () {
+						return curVal;
+					}
+				});
+			});
+		}
+
+		callbacks.push(callback);
+
+		return {
+			remove: function () {
+				var idx = callbacks.indexOf(callback);
+				if (idx >= 0) {
+					callbacks.splice(idx, 1);
+				}
+			}
+		};
+	}
+
+	////////////////////////////////////////////////////////////////////////
+	// Code to collect changes and deliver summary at end of microtask.
+	////////////////////////////////////////////////////////////////////////
+
 	// Keep track of order that callbacks were registered, we will call them in that order
 	// regardless of the order the objects were updated.
 	var seq = 0;
@@ -25,7 +135,7 @@ define([
 			this.oldVals = {};
 		},
 
-		onChange: function(prop, oldVal) {
+		onChange: function (prop, oldVal) {
 			if (!(prop in this.oldVals)) {
 				this.oldVals[prop] = oldVal;
 				hotChangeCollectors[this._seq] = this;
@@ -71,63 +181,6 @@ define([
 		deliverHandle = null;
 	}
 
-	// Object.is() polyfill from Observable.js
-	function is(lhs, rhs) {
-		return lhs === rhs && (lhs !== 0 || 1 / lhs === 1 / rhs) || lhs !== lhs && rhs !== rhs;
-	}
-
-	// Mapping from instrumented object to array of listeners to notify when that object is changed.
-	var map = new WeakMap();
-
-	// Call callback(prop, oldVal, newVal) whenever a property or nested property of the POJO is changed.
-	// Call is synchronous.
-	function watchPojo(pojo, callback) {
-		// Functions to call whenever a property of the POJO is changed.
-		var watchers = map.get(pojo);
-
-		if (!watchers) {
-			watchers = [];
-			map.set(pojo, watchers);
-
-			// Go through each property in the object, and convert it to a custom setter and getter.
-			Object.keys(pojo).forEach(function (prop) {
-				// Shadow value referenced by setters and getters.
-				var curVal = pojo[prop];
-
-				Object.defineProperty(pojo, prop, {
-					enumerable: true,
-
-					set: function (newVal) {
-						if (is(newVal, pojo[prop])) {
-							return;
-						}
-
-						watchers.forEach(function (watcher) {
-							watcher(prop, curVal, newVal);
-						});
-
-						curVal = newVal;
-					},
-
-					get: function () {
-						return curVal;
-					}
-				});
-			});
-		}
-
-		watchers.push(callback);
-
-		return {
-			remove: function () {
-				var idx = watchers.indexOf(callback);
-				if (idx >= 0) {
-					watchers.splice(idx, 1);
-				}
-			}
-		}
-	}
-
 	// Call `callback(oldVals)` whenever one or more properties on specified POJO are changed.
 	function observePojo(pojo, callback) {
 		var changeCollector = new ChangeCollector(callback);
@@ -135,9 +188,8 @@ define([
 	}
 
 	/**
-	 * Call `callback(oldVals)` whenever one or more properties on specified object are changed.
+	 * Call `callback(oldVals)` whenever one or more properties or nested properties on specified object are changed.
 	 * Object can be a POJO or a decor/Stateful subclass (i.e. an Object with an `observe()` method).
-	 *
 	 * Similar to decor/Observable and decor/Stateful, but with key difference that it
 	 * attaches to an existing object.
 	 *
@@ -152,13 +204,12 @@ define([
 	 * of the order the objects are updated in.
 	 * (TODO: that won't really be true until this module and decor/Stateful (or decor/Observable)
 	 * share the code for delivering the change notifications)
-	 *
 	 */
 	return function (obj, callback) {
 		if (typeof obj.observe === "function") {
 			return obj.observe(callback);
 		} else {
-			observePojo(obj, callback);
+			return observePojo(obj, callback);
 		}
 	};
 });
--- tests/unit/observe.js
+++ tests/unit/observe.js
@@ -1,12 +1,14 @@
 define([
 	"intern!object",
 	"intern/chai!assert",
-	"decor/observe"
-], function (registerSuite, assert, observe) {
+	"dcl/dcl",
+	"decor/observe",
+	"decor/Stateful"
+], function (registerSuite, assert, dcl, observe, Stateful) {
 	var handles = [];
 
 	registerSuite({
-		name: "bind",
+		name: "observe",
 
 		"basic": function () {
 			var dfd = this.async(1000);
@@ -67,6 +69,74 @@ define([
 			}), 1);
 		},
 
+		"set to same value": function () {
+			var dfd = this.async(1000);
+			var log = [];
+			var pojo = {
+				foo: "Foo0",
+				bar: "Bar0"
+			};
+			handles.push(observe(pojo, function (oldVals) {
+				log.push(oldVals);
+			}));
+
+			pojo.foo = "Foo0";
+
+			setTimeout(dfd.callback(function () {
+				assert.deepEqual(log, [], "no notifications");
+			}), 1);
+		},
+
+		"notifications collapsed": function () {
+			var dfd = this.async(1000);
+			var log = [];
+			var pojo = {
+				foo: "Foo0",
+				bar: "Bar0"
+			};
+			handles.push(observe(pojo, function (oldVals) {
+				log.push(oldVals);
+			}));
+
+			pojo.foo = "Foo1";
+			pojo.foo = "Foo2";
+
+			setTimeout(dfd.callback(function () {
+				assert.deepEqual(log, [{
+					foo: "Foo0"
+				}], "only one notification");
+			}), 1);
+		},
+
+		"unobserve": function () {
+			var dfd = this.async(1000);
+			var log = [];
+			var pojo = {
+				foo: "Foo0",
+				bar: "Bar0"
+			};
+			var handle = observe(pojo, function (oldVals) {
+				log.push(oldVals);
+			});
+
+			pojo.foo = "Foo1";
+
+			setTimeout(dfd.rejectOnError(function () {
+				assert.deepEqual(log, [{
+					foo: "Foo0"
+				}], "observed");
+
+				handle.remove();
+				log = [];
+
+				pojo.foo = "Foo2";
+
+				setTimeout(dfd.callback(function () {
+					assert.deepEqual(log, [], "no notifications");
+				}), 1);
+			}), 1);
+		},
+
 		"multiple observers": function () {
 			var dfd = this.async(1000);
 			var log = [];
@@ -105,10 +175,141 @@ define([
 					], "second notification");
 				}), 1);
 			}), 1);
-		}
+		},
 
-		// TODO: check that observers called in order they were registered.
-		// TODO: test observe of decor/Stateful subclasses
-		// TODO: test that setting a prop to its current value doesn't trigger a notification
+		"observers called in order registered": function () {
+			var dfd = this.async(1000);
+			var log = [];
+			var pojo1 = {
+				foo: "pojo 1",
+				bar: "Bar0"
+			};
+			var pojo2 = {
+				foo: "pojo 2",
+				bar: "Bar0"
+			};
+
+			handles.push(observe(pojo1, function (oldVals) {
+				log.push(oldVals);
+			}));
+			handles.push(observe(pojo2, function (oldVals) {
+				log.push(oldVals);
+			}));
+
+			// Update pojo2 first.  Pojo1 listener should still be called first.
+			pojo2.foo = "pojo 2 update";
+			pojo1.foo = "pojo 1 update";
+
+			setTimeout(dfd.callback(function () {
+				assert.deepEqual(log, [
+					{
+						foo: "pojo 1"
+					},
+					{
+						foo: "pojo 2"
+					}
+				]);
+			}), 1);
+		},
+
+		"nested": function () {
+			var dfd = this.async(1000);
+			var log = [];
+			var pojo = {
+				foo: "Foo0",
+				bar: "Bar0",
+				nested1: {
+					a: 1,
+					b: 2,
+					nested2: {
+						c: 3,
+						d: 4
+					}
+				}
+			};
+			handles.push(observe(pojo, function (oldVals) {
+				log.push(oldVals);
+			}));
+
+			// Change nested property.
+			pojo.nested1.a = 2;
+
+			setTimeout(dfd.rejectOnError(function () {
+				assert.deepEqual(log, [
+					{
+						"nested1.a": 1
+					}
+				], "change in nested1");
+
+				assert.strictEqual(pojo.nested1.a, 2, "new nested1.a");
+
+				log = [];
+
+				pojo.nested1.nested2.c = 4;
+
+				setTimeout(dfd.rejectOnError(function () {
+					assert.deepEqual(log, [
+						{
+							"nested1.nested2.c": 3
+						}
+					], "changed in nested2");
+
+					log = [];
+
+					// Change whole object.
+					var oldNested1 = pojo.nested1;
+					pojo.nested1 = {
+						a: 5,
+						b: 2,
+						nested2: {
+							c: 4,
+							d: 10
+						}
+					};
+
+					setTimeout(dfd.rejectOnError(function () {
+						assert.deepEqual(log, [
+							{
+								"nested1.a": 2,
+								"nested1.nested2.d": 4
+							}
+						], "new nested1");
+
+						log = [];
+
+						// Check that changes to new nested1 are tracked and conversely,
+						// that changes to old nested1 are *not* tracked.
+						oldNested1.a = 20;
+						pojo.nested1.b = 30;
+
+						setTimeout(dfd.callback(function () {
+							assert.deepEqual(log, [
+								{
+									"nested1.b": 2
+								}
+							], "changes to new nested1 tracked, changes to old nested1 not tracked");
+						}), 1);
+					}), 1);
+				}), 1);
+			}), 1);
+		},
+
+		"observe decor/Stateful subclass": function () {
+			var dfd = this.async(1000);
+			var MyClass = dcl(Stateful, {
+				foo: 1,
+				bar: 2
+			});
+			var myObj = new MyClass();
+			handles.push(observe(myObj, dfd.callback(function (oldVals) {
+				assert.deepEqual(oldVals, {
+					foo: 1,
+					bar: 2
+				}, "oldVals");
+			})));
+
+			myObj.foo = 3;
+			myObj.bar = 4;
+		}
 	});
 });
