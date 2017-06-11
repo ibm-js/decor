7f101cb917f7175c2f6176420d0ef4608c0813a4
Bill Keese <bill@dojotoolkit.org>
2017-06-11 14:28:04 +0900

    Fix sniff() test to run on Mac (in addition to Windows).

--- tests/functional/sniff.js
+++ tests/functional/sniff.js
@@ -2,7 +2,7 @@ define(["intern",
 	"intern!object",
 	"intern/dojo/node!leadfoot/helpers/pollUntil",
 	"intern/chai!assert",
-	"require",
+	"require"
 ], function (intern, registerSuite, pollUntil, assert, require) {
 	var PAGE = "./sniff.html";
 
@@ -19,50 +19,56 @@ define(["intern",
 						intern.config.WAIT_TIMEOUT, intern.config.POLL_INTERVAL))
 				.execute("return _has")
 				.then(function (has) {
-					function shouldDetectOnly(expected) {
-						Object.keys(has).forEach(function (browser) {
-							if (expected.indexOf(browser) > -1) {
-								assert.ok(has[browser], "detected " + browser + " as expected");
-							} else {
-								assert.notOk(has[browser], "didn't detect " + browser + " as expected");
-							}
-						});
-					}
-
 					var browserName = remote.environmentType.browserName,
 						platform = remote.environmentType.platform,
 						deviceName = remote.environmentType.deviceName;
+
+					// Check platform flags.
+					if (/mac/i.test(platform) && browserName !== "iOS") {
+						assert(has.mac, "has(mac) set");
+					} else {
+						assert.isFalse(has.mac, "has(mac) not set");
+					}
+
+					// Check browser flags.
+					function shouldDetectOnlyBrowsers(expected) {
+						Object.keys(has).forEach(function (flags) {
+							if (flags === "mac") {
+								return;
+							}
+							if (expected.indexOf(flags) > -1) {
+								assert.ok(has[flags], "detected " + flags + " as expected");
+							} else {
+								assert.notOk(has[flags], "didn't detect " + flags + " as expected");
+							}
+						});
+					}
 					switch (browserName) {
 					case "chrome":
-						shouldDetectOnly(["chrome", "webkit"]);
+						shouldDetectOnlyBrowsers(["chrome", "webkit"]);
 						break;
 					case "internet explorer":
-						shouldDetectOnly(["ie"]);
+						shouldDetectOnlyBrowsers(["ie"]);
 						break;
 					case "microsoftedge":	// TODO: Check actual value of browserName once Intern supports Edge.
-						shouldDetectOnly(["edge"]);
+						shouldDetectOnlyBrowsers(["edge"]);
 						break;
 					case "firefox":
-						shouldDetectOnly(["ff", "mozilla"]);
+						shouldDetectOnlyBrowsers(["ff", "mozilla"]);
 						break;
 					case "safari":
-						switch (platform) {
-						case "MAC":
-							shouldDetectOnly(["safari", "webkit", "mac"]);
-							break;
-						}
+						shouldDetectOnlyBrowsers(["safari", "webkit"]);
 						break;
 					case "iOS":
 						switch (deviceName) {
 						case "iPad Simulator":
-							shouldDetectOnly(["safari", "webkit", "ios", "ipad"]);
+							shouldDetectOnlyBrowsers(["safari", "webkit", "ios", "ipad"]);
 							break;
 						case "iPhone Simulator":
-							shouldDetectOnly(["safari", "webkit", "ios", "iphone"]);
+							shouldDetectOnlyBrowsers(["safari", "webkit", "ios", "iphone"]);
 							break;
 						}
 						break;
-
 					}
 				})
 				.end();
